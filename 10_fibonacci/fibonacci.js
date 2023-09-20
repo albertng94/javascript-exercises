74a748bc92bdf0ebc270197c94829a0d838d2662
albertng94 <albertng94@gmail.com>
2023-09-20 12:12:41 +0200

    10-fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    let fibonacci = [1, 1];
+    for (let i = 0; i < Number(index)-2; i++) {
+        fibonacci.push(fibonacci[(fibonacci.length-1)]+fibonacci[(fibonacci.length-2)]);
+    }
+    if (Number(index) >= 0) {
+        return fibonacci[fibonacci.length-1];
+    } else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
